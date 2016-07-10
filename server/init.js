86a15eff97953bf83f5e1e3e73344f4d87003ce4
Vijay das Manikpuri <vijaydas.mp@gmail.com>
2016-07-11 00:50:47 +0530

    navbar update and code cleanup in init code

--- server/init.js
+++ server/init.js
@@ -1,16 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-  /*smtp = {
-    username: 'postmaster@bikemitra.com',
-    password: '56ea81e20c7deb65193244604da6138d',
-    server:   'smtp.mailgun.org',  // eg: mail.gandi.net
-    port: 587
-  }*/
-  //process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
-  username = Meteor.settings.mailgun.username;
-  
+Meteor.startup(() => {  
    process.env.MAIL_URL = 'smtp://'+Meteor.settings.mailgun.username+':'+Meteor.settings.mailgun.password+'@'+Meteor.settings.mailgun.server+':'+'587'
    console.log("MAIL_URL  "+process.env.MAIL_URL);
 });
