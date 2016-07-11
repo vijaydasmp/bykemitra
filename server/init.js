2fdb6d056d7d196c182ac071e88e46e1eafeabda
Vijay das Manikpuri <vijaydas.mp@gmail.com>
2016-07-11 07:43:33 +0530

    updated init.js

--- server/init.js
+++ server/init.js
@@ -1,7 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {  
-   process.env.MAIL_URL = 'smtp://'+Meteor.settings.mailgun.username+':'+Meteor.settings.mailgun.password+'@'+Meteor.settings.mailgun.server+':'+'587'
+   //process.env.MAIL_URL = 'smtp://'+Meteor.settings.mailgun.username+':'+Meteor.settings.mailgun.password+'@'+Meteor.settings.mailgun.server+':'+'587'
+    process.env.MAIL_URL  = 'smtp://postmaster@bikemitra.com:56ea81e20c7deb65193244604da6138d@smtp.mailgun.org:587';
    console.log("MAIL_URL  "+process.env.MAIL_URL);
 });
 
